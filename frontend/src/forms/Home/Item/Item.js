af49940d3ee8db994b4b2f1b89e40f7ceee24113
apisitprompha <apisitprompha@gmail.com>
2020-02-15 09:20:49 +0000

    ADD FINISH TAB_x2

--- frontend/src/forms/Home/Item/Item.js
+++ frontend/src/forms/Home/Item/Item.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
+
 export default class Item extends Component {
     render() {
         return (
             <div>
+                <h1>Item</h1>
             </div>
         )
     }
--- frontend/src/views/Pages/Home/Home.js
+++ frontend/src/views/Pages/Home/Home.js
@@ -1,53 +1,45 @@
 import React, { Component } from 'react'
-import { Card, Button } from 'antd'
-import HomeForm from '../../../forms/Home/HomeForm'
-import AboutUsForm from '../../../forms/About/AboutUsForm'
-import Product from '../../../forms/Product/Product'
+import { Card, Input, Tag, Button, Icon } from 'antd'
+import { Image } from 'react-bootstrap'
+import { Row, Col } from 'reactstrap'
+import goodshop_x2 from '../../../public/img/goodshop-X2.jpg'
+const { Search } = Input;
 export default class Home extends Component {
-    state = {
-      key: 'tab1',
-      noTitleKey: 'home',
-    };
-    onTabChange = (key, type) => {
-      console.log(key, type);
-      this.setState({ [type]: key });
-    };
-    render() {
-        const tabListNoTitle = [
-          {
-            key: 'product',
-            tab: 'PRODUCT & GOODS',
-          },
-          {
-            key: 'home',
-            tab: `HOME PAGE`,
-          },
-          {
-            key: 'about',
-            tab: 'ABOUT US',
-          },
-          
-        ];
-        
-        const contentListNoTitle = {
-            product: <Product />,
-            home: <HomeForm />,
-            about: <AboutUsForm />,
-        };
-        return (
-            <div>
-                <Card
-                  style={{ width: '100%'}}
-                  tabList={tabListNoTitle}
-                  activeTabKey={this.state.noTitleKey}
-                  tabBarExtraContent={<Button shape="round">SIGN IN</Button>}
-                  onTabChange={key => {
-                    this.onTabChange(key, 'noTitleKey');
-                  }}
-                >
-                  {contentListNoTitle[this.state.noTitleKey]}
-                </Card>
-            </div>
-        )
+  render() {
+    const iconStype = {
+      fontSize: '200%', 
+      paddingLeft: '1%'
     }
+    return (
+      <div>
+          <Card style={{ width: '100%' }}>
+            <Row>
+              <Col md="3" xs="12" sm="6">
+                <Image width="200px" height="80px" src={goodshop_x2} rounded />
+              </Col>
+              <Col md="6">
+                <Search placeholder="ค้นหาสิ่งที่ต้องการ" onSearch={value => console.log(value)} enterButton />
+                    <div style={{paddingTop: '12px'}}>
+                    <Tag>พยุง</Tag>
+                    <Tag>ยางนา</Tag>
+                    <Tag>ของใช้ในบ้าน</Tag>
+                    <Tag>สักทอง</Tag>
+                    <Tag>ไม้ผล</Tag>
+                    <Tag>พืชเก็บเกี่ยว</Tag>
+                    <Tag>อื่น ๆ</Tag>
+                  </div>
+              </Col>
+              <Col md="1">
+                <Icon style={iconStype} type="bell" theme="filled" />
+                <Icon style={iconStype} type="wechat" theme="filled" />
+              </Col>
+              <Col md="2">
+                <Button size="small" shape="round">สมัคร|ใช้งาน</Button>  
+                <Button size="small" shape="round">ขาย</Button>  
+              </Col>
+            </Row>
+          </Card>
+      </div>
+    )
+  }
 }
